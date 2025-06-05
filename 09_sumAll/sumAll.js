ae6877f5c1457c5adec87f9c911e2d65a33aba5d
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-05 18:12:28 -0400

    Refactor 9th exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,19 +1,17 @@
 const sumAll = function(a, b) {
-    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) 
+        || a < 0 || b < 0) {
         return "ERROR";
     }
+
+    // Swap order of args if first is > second
+    if (a > b) [b, a] = [a, b];
+
     let sum = 0;
-    if (a < b) {
-        for (let i = a; i <= b; i++) {
-            sum += i;
-        }
-        return sum;
-    } else {
-        for (let i = b; i <= a; i++) {
-            sum += i;
-        }
-        return sum;
+    for (let i = a; i <= b; i++) {
+        sum += i;
     }
+    return sum;
 };
 
 // Do not edit below this line
