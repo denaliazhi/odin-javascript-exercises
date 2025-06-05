0906a9905143a125414861535fb0183b71f72ae0
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-05 18:21:43 -0400

    Refactor 10th exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,9 +1,9 @@
 const leapYears = function(year) {
     // If year is divisible by 4
-    if (year % 4 == 0
-        // It must be divisible by both 100 and 400
-        && ((year % 100 == 0 && year % 400 == 0)
-            // OR It must not be divisible by 100
+    if (year % 4 === 0
+        // It must be divisible by 400
+        && ((year % 400 === 0)
+            // OR it must not be divisible by 100
             || (year % 100 != 0))
     ) {
         return true;
