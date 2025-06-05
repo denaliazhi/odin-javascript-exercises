3e65c09597a57e73fcb312a2d41955eac29a3ad7
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-05 17:48:24 -0400

    Complete 8th exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < arr.length; i++) {
+        if (args.includes(arr[i])) {
+            // Modify array in-place
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
