01318c4dd8261c230382760a2266447a90b7408b
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-05 18:18:54 -0400

    Complete 10th exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // If year is divisible by 4
+    if (year % 4 == 0
+        // It must be divisible by both 100 and 400
+        && ((year % 100 == 0 && year % 400 == 0)
+            // OR It must not be divisible by 100
+            || (year % 100 != 0))
+    ) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
