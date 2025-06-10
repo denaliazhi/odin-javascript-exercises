b449e3fcc22e086ac6f1677427091f0eb491ae54
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-10 12:57:35 -0400

    Complete 12th exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((tot, curr) => tot + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((tot, curr) => tot * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (n; n > 0; n--){
+    product *= n;
+  }
+	return product;
 };
 
 // Do not edit below this line
