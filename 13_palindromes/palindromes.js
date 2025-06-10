53c64a48ba87f92a66ea8d53ccce1f37b6a27ccf
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-10 13:23:27 -0400

    Complete 13th exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // Non-alphanumerics shouldn't be considered in palindrome
+    let tidied = word.replace(/[^a-zA-Z0-9]/g, '')
+    let midpoint = tidied.length / 2;
+    for (let front = 0; front < midpoint; front++) {
+        let back = tidied.length - (front + 1);
+        if (tidied[front].toLowerCase() !== tidied[back].toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
