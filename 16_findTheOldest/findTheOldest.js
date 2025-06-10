318612c535934f2228f3c0dea49d972d8c6e73db
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-10 15:08:07 -0400

    Complete 16th exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Calculate age for each person
+    people.forEach(person => {
+        person.yearOfDeath ??= new Date().getFullYear();
+        person['age'] = person.yearOfDeath - person.yearOfBirth;
+    })
 
+    // Sorts by oldest descending
+    return people.sort((a,b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
